41873e00059953ebb127278b83c163368015d93b
Stig_XD <phonex9005@gmail.com>
2026-05-11 14:59:07 +0400

    Practice 3 finished

---
+++ Practice3/Practice3.Task10/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace Practice3.Task10;
+public class Program
+{
+    static void Main()
+    {
+        var car = new Car { Model = "Toyota LandCruiser 200" };
+
+        PrintModel(car);
+
+        Console.ReadKey();
+    }
+
+    public static void PrintModel(Car car)
+    {
+        Console.WriteLine($"Марка автомобиля: {car.Model}");
+    }
+
+}
+
+public class Car
+{
+    public string Model { get; set; }
+}
---
+++ Practice3/Practice3.Task6/Program.cs
@@ -0,0 +1,27 @@
+﻿using System.Net.WebSockets;
+using System.Reflection.Metadata.Ecma335;
+
+namespace Practice3.Task6;
+
+public class Program
+{
+    static void Main()
+    {
+        var point1 = new Point { X = 10, Y = 3 };
+        var point2 = new Point { X = 0, Y = 6 };
+
+        Console.WriteLine(Distance(point1, point2));
+        Console.ReadKey();
+    }
+
+    public static double Distance(Point point1, Point point2)
+    {
+        return Math.Sqrt(Math.Pow(point1.X - point2.X, 2) + Math.Pow(point1.Y - point2.Y, 2));
+    }
+}
+
+public struct Point
+{
+    public int X { get; set; }
+    public int Y { get; set; }
+}
